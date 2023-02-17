b1b1956d1d03bf3f305ac0598b5601a31ff10e38
marinivosevic <95760234+marinivosevic@users.noreply.github.com>
2023-02-17 22:05:06 +0100

    Cart done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
-import {AiOutlineShipping} from 'react-icons';
+import { AiOutlineShopping } from 'react-icons/ai'
+
+import { Cart } from './';
+import { useStateContext} from '../context/StateContext';
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
-    <div className='navbar-container'>
-      <p className='logo'>
-        <Link href="/">Diguzor macka</Link>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Zmay World</Link>
       </p>
 
-      <button type='button' className='cart-icon' onClick="">
-        <AiOutlineShipping/>
-        <span className='cart-item-qty'>
-        1
-        </span>
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,29 @@ import { Toaster } from "react-hot-toast";
 import Layout1 from "../components";
 import Head from "next/head";
 
-import Navbar from "../components";
-import {Footer} from "../components";
+import {Navbar} from "../components";
+import { Footer } from "../components";
 import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
+
+
 
 function MyApp({ Component, pageProps }) {
+  
   return (
-    <div >
-     
-     
-      <main className="main-container">
-        <Toaster />
-        <Component {...pageProps} />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+    <div>
+      <StateContext>
+       <Navbar/>
+
+        <main className="main-container">
+          <Toaster />
+
+          <Component {...pageProps} />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </StateContext>
     </div>
   );
 }
